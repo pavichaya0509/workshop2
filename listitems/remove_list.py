f27b111792938f3b30cc73bcb3a27e29924647dc
pavichaya0509 <pavichaya.so@gmail.com>
2021-01-17 09:38:41 +0700

    Add 3 folder

---
+++ listitems/remove_list.py
@@ -0,0 +1,19 @@
+# Ex1
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+# Ex2
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+# Ex3
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+# Ex4
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+# Ex5
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
---
+++ listitems/sort_list.py
@@ -0,0 +1,8 @@
+#Ex1
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+#Ex2
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse=True)
+print(thislist)
